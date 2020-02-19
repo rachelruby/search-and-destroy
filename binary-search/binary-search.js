bb483348c59b800f04442216e4ff792be5003b51
Rachel Burrous <rachelashleyburrous@gmail.com>
2020-02-19 15:43:25 -0500

    binary search done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,21 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if (array.length < 1) {
+		return false
+	}
+	if (array.length === 1) {
+		return array[0] === target
+	} else {
+		const midpoint = Math.floor(array.length/2)
+		const left = array.slice(0, midpoint)
+		const right = array.slice(midpoint)
+		if(target < right[0]) {
+			return binarySearch(left, target)
+		} else {
+			return binarySearch(right, target)
+		}
+	}
 };
 
 /*
